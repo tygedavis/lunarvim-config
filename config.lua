1619484b2e5e5eee55014dfd08700ee469a6d986
tygedavis <tdavis@cmsonline.com>
2022-02-22 09:34:15 -0700

    Adding new colorscheme

--- config.lua
+++ config.lua
@@ -11,7 +11,8 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.colorscheme = "onedarker"
+lvim.colorscheme = "codedark"
+
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
@@ -138,23 +139,11 @@ linters.setup {
 }
 
 -- Additional Plugins
--- lvim.plugins = {
---     {"folke/tokyonight.nvim"},
---     {
---       "folke/trouble.nvim",
---       cmd = "TroubleToggle",
---     },
--- }
+lvim.plugins = {
+    { "tomasiser/vim-code-dark" }
+}
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- lvim.autocommands.custom_groups = {
 --   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
 -- }
-vim.cmd([[
-    augroup cursor_off
-        autocmd!
-        autocmd WinLeave * set nocursorline nocursorcolumn
-        autocmd WinEnter * set cursorline cursorcolumn
-    augroup END
-
-]])
